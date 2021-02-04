ab9c3b90817d725f43440cc7318ae23f12221334
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-02-04 12:42:07 -0300

    feat: implementing auth service

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,1 +1,42 @@
-// TODO
+// import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import IUserRepository from 'repositories/models/IUsersRepository'
+import User from '../database/entities/User'
+import AppError from '../errors/AppError'
+import CryptoConfig from '../config/CryptographyConfig'
+
+interface RequestDTO {
+  email: string
+  password: string
+}
+
+interface ResponseDTO {
+  user: User
+  token: string
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository
+  ) {}
+
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('incorrect email/password combination', 401)
+    }
+
+    const crypto = new CryptoConfig()
+    const passwordMatch = await crypto.compareHash(password, user.password)
+
+    if (!passwordMatch) {
+      throw new AppError('incorrect email/password', 401)
+    }
+
+    // still needs the JWT part
+  }
+}
