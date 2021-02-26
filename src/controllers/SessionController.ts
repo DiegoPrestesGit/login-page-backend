068328bcabf63a2c40fa7dff83f5ce4d93a7e9eb
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-02-26 17:11:17 -0300

    feat: controller for jwt verification is created

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata'
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import AuthenticateUser from '../services/AuthenticateUserService'
+import AuthorizeUser from '../services/AuthorizeUserService'
+import AppError from '../errors/AppError'
 
 export default class UserController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -22,10 +24,17 @@ export default class UserController {
 
   public async show(request: Request, response: Response): Promise<Response> {
     try {
-      const xqdl = request.headers.authorization
-      console.log(xqdl)
+      const { authorization } = request.headers
 
-      return response.json({})
+      if (!authorization) {
+        throw new AppError('Your token has inspired', 401)
+      }
+      const [_, token] = authorization?.split(' ')
+
+      const authorizeUser = container.resolve(AuthorizeUser)
+      const authorizer = await authorizeUser.execute(token)
+
+      return response.json(authorizer)
     } catch (err) {
       return response.status(err.statusCode).json({
         status: err.statusCode,
